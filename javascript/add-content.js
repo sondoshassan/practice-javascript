3445095db0e4e177026565ae5d31e4a83265d3b3
sondoshassan <sondos199628@gmail.com>
2020-02-04 20:37:05 +0200

    add javascript

---
+++ javascript/add-content.js
@@ -0,0 +1,16 @@
+var today = new Date();
+var hournow = today.getHours();
+var greeting;
+if (hournow > 18){
+    greeting = 'good evening!';
+}
+else if (hournow > 12){
+    greeting = 'good afternoon!';
+}
+else if (hournow > 0){
+    greeting = 'good morning!';
+}
+else {
+    greeting = 'welcome!';
+}
+document.write ('<h3>' + greeting + '</h3>');
