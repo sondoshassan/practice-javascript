206b61dce0d461116ad051a5fa5d04a73102d705
sondoshassan <sondos199628@gmail.com>
2020-02-05 13:07:20 +0200

    make function

--- javascript/add-content.js
+++ javascript/add-content.js
@@ -1,16 +1,23 @@
 var today = new Date();
-var hournow = today.getHours();
-var greeting;
-if (hournow > 18){
-    greeting = 'good evening!';
+function time (){
+    //input
+    var hournow = prompt ('enter the time now','it will be 0-24');
+    var greeting;
+    //process
+    if (hournow >= 18 && hournow <= 24){
+        greeting = 'good evening!';
+    }
+    else if (hournow >= 12 && hournow < 18){
+        greeting = 'good afternoon!';
+    }
+    else if (hournow >= 0 && hournow < 12){
+        greeting = 'good morning!';
+    }
+    else {
+        greeting = 'welcome!';
+    }
+    //output
+    return greeting;
 }
-else if (hournow > 12){
-    greeting = 'good afternoon!';
-}
-else if (hournow > 0){
-    greeting = 'good morning!';
-}
-else {
-    greeting = 'welcome!';
-}
+var greeting = time();
 document.write ('<h3>' + greeting + '</h3>');
